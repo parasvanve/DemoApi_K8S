7a156a5acaca4e71adfa6108143870b4fa775a80
parasvanve <parasvanve218@gmail.com>
2026-09-25 19:34:23 +0530

    Update Program.cs

--- DemoApi_K8S/Program.cs
+++ DemoApi_K8S/Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 
 app.MapGet("/", () =>
 {
-    return "Hello from Kubernetes - Version 4! Hello-------------------------------------------everyone ";
+    return "Hello from Kubernetes - Version 4! Hello-------------------------------------------everyone---------------- ";
     
 });
 
